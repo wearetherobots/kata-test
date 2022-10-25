00a64b9b095e7563806f587647cc620a8a9f5aa0
MisaelMa <amisael.amir.misael@gmail.com>
2022-10-25 09:42:23 -0500

    feat(printers): se finalizo el arbol en forma de text

--- src/CategoryTree/Printer.php
+++ src/CategoryTree/Printer.php
@@ -6,9 +6,23 @@ class Printer
 {
     public function build(Category $parent, int $spaces = 0): string
     {
-        /**
-         * @todo
-         */
-        return '';
+        return $this->getTree($parent);
+    }
+
+    public function getTree(Category $parent, int $spaces = 0)
+    {
+        $children = $parent->children();
+        // si esta vacio se agrega el spacio con el nombre
+        if (empty($children)) {
+            return str_repeat('  ', $spaces++) . $parent->name() . PHP_EOL;
+        } else {
+            $stringTree = str_repeat('  ', $spaces++) . $parent->name() . PHP_EOL;
+            usort($children, fn ($a, $b) => $a->name() <=> $b->name());
+            foreach ($children as $child) {
+                // se hace uso de lo recursivo
+                $stringTree = $stringTree . $this->getTree($child, $spaces);
+            }
+            return $stringTree;
+        }
     }
 }
--- src/UniqueArray/DuplicateRemover.php
+++ src/UniqueArray/DuplicateRemover.php
@@ -9,6 +9,23 @@ class DuplicateRemover
         /**
          * @todo
          */
-        return [];
+        $unique = [];
+        foreach ($input as $data) {
+            if (!$this->findInUnique($unique, $data)) {
+                $unique[] = $data;
+            }
+        }
+        return $unique;
+    }
+
+    public function findInUnique(array $uniqueArr, int $n): bool
+    {
+        $exist = false;
+        foreach ($uniqueArr as $val) {
+            if ($n == $val) {
+                $exist = true;
+            }
+        }
+        return $exist;
     }
 }
