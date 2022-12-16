e6a391ff2e44973384f948682530ffcf3352cf9e
Andres <andrestheran@outlook.com>
2022-12-16 12:43:57 -0500

    Commit son la solucion de los ejercicios

--- src/CategoryTree/Printer.php
+++ src/CategoryTree/Printer.php
@@ -6,9 +6,12 @@ class Printer
 {
     public function build(Category $parent, int $spaces = 0): string
     {
-        /**
-         * @todo
-         */
-        return '';
+        $tree = str_repeat('  ', $spaces) . $parent->getName() . PHP_EOL;
+
+        foreach ($parent->getChildren() as $child) {
+            $tree .= $this->build($child, $spaces + 1);
+        }
+
+        return $tree;
     }
 }
